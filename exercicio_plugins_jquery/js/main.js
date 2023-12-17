3a9595d8bba11b3d71f2a04f090bfbc2fa45aaa3
Gustavo Dias <gustavoalvesdiaz08@gmail.com>
2023-12-17 11:46:59 -0300

    exercicio finalizado

--- exercicio_plugins_jquery/js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(() => {
-  const cpf = $("#cpf");
-  const telefone = $("#telefone");
-  const cep = $("#cep");
-
-  cpf.mask("000.000.000-00");
-  telefone.mask("(00) 00000-0000");
-  cep.mask("00000-000");
-});
