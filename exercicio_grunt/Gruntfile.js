6541ba350c56a1c33ba8f1a7c5722a00943ca3e5
Gustavo Dias <gustavoalvesdiaz08@gmail.com>
2024-01-02 20:50:45 -0300

    Commit inicial

---
+++ exercicio_grunt/Gruntfile.js
@@ -0,0 +1,9 @@
+module.exports = function (grunt) {
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-contrib-htmlmin");
+  grunt.loadNpmTasks("grunt-contrib-less");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-text-replace");
+
+  grunt.initConfig({});
+};
