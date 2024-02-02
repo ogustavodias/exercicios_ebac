6093c2d2b4d98f7a829564faec84641d317736de
Gustavo Dias <gustavoalvesdiaz08@gmail.com>
2024-02-02 07:47:48 -0300

    exercicio finalizado

---
+++ exercicio_ts/dist/main.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiply(n1, n2) {
+    return n1 * n2;
+}
+function hello(nome) {
+    return "Ol\u00E1 ".concat(nome);
+}
+console.log(multiply(4, 4));
+console.log(hello("Gustavo"));
---
+++ exercicio_ts/main.ts
@@ -0,0 +1,10 @@
+function multiply(n1: number, n2: number): number {
+  return n1 * n2;
+}
+
+function hello(nome: string): string {
+  return `Olá ${nome}`;
+}
+
+console.log(multiply(4, 4));
+console.log(hello("Gustavo"));
